322dabd9be75cdf19f0f8f607aa83a6e9d4daafe
toropippi <44022497+toropippi@users.noreply.github.com>
2020-12-09 15:50:07 +0900

    bubble実装

---
+++ OceanFFT6Bubbles/Assets/Scripts/FFTOceanMain.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+
+public class FFTOceanMain : MonoBehaviour
+{
+    [SerializeField] ComputeShader shaderGenerateSpectrum;
+    [SerializeField] ComputeShader shaderSetNormal;
+    [SerializeField] Shader renderingShader;
+    Material renderingShader_Material;
+    [SerializeField] FFT fFT;
+    [SerializeField] Phillips phillips;
+    int kernel_GenerateSpectrumKernel, kernel_SetNormalBubble;
+    Vector2[] h_h0;
+    public ComputeBuffer d_h0, d_ht, d_ht_dx, d_ht_dz, d_ht_dmy, d_displaceX, d_displaceZ;//dmyは一時計算用
+    RenderTexture normal_Tex, bubble_Tex;
+    int cnt;
+    [SerializeField] float lambda = -2.0f;
+
+    private void Awake()
+    {
+        kernel_GenerateSpectrumKernel = shaderGenerateSpectrum.FindKernel("GenerateSpectrumKernel");
+        kernel_SetNormalBubble = shaderSetNormal.FindKernel("SetNormalBubble");
+        renderingShader_Material = new Material(renderingShader);
+        cnt = 0;
+    }
+
+    void Start()
+    {
+        normal_Tex = CreateRenderTeture(RenderTextureFormat.ARGBFloat);
+        bubble_Tex = CreateRenderTeture(RenderTextureFormat.RFloat);
+        h_h0 = phillips.Generate_h0();//CPU側メモリ確保。サイズはsizeof(Vector2)*256*256
+        d_h0 = new ComputeBuffer(h_h0.Length, sizeof(float) * 2);//GPU側メモリ確保 サイズはsizeof(Vector2)*256*256
+        d_ht = new ComputeBuffer((int)(Phillips.meshSize * Phillips.meshSize), sizeof(float) * 2);//GPU側メモリ確保 サイズはsizeof(Vector2)*256*256
+        d_ht_dx = new ComputeBuffer((int)(Phillips.meshSize * Phillips.meshSize), sizeof(float) * 2);//x偏微分用
+        d_ht_dz = new ComputeBuffer((int)(Phillips.meshSize * Phillips.meshSize), sizeof(float) * 2);//z偏微分用
+        d_displaceX = new ComputeBuffer((int)(Phillips.meshSize * Phillips.meshSize), sizeof(float) * 2);//ディスプレスX choppy wave用
+        d_displaceZ = new ComputeBuffer((int)(Phillips.meshSize * Phillips.meshSize), sizeof(float) * 2);//ディスプレスZ choppy wave用
+        d_ht_dmy = new ComputeBuffer(d_ht.count, sizeof(float) * 2);
+        d_h0.SetData(h_h0);
+        SetArgs();
+    }
+
+    RenderTexture CreateRenderTeture(UnityEngine.RenderTextureFormat format)
+    {
+        var tex = new RenderTexture((int)Phillips.meshSize, (int)Phillips.meshSize, 0, format);
+        tex.enableRandomWrite = true;
+        tex.Create();
+        tex.filterMode = FilterMode.Bilinear;
+        //tex.filterMode = FilterMode.Trilinear;
+        tex.wrapMode = TextureWrapMode.Repeat;
+        //tex.wrapMode = TextureWrapMode.Clamp;
+        return tex;
+    }
+
+    void SetArgs()
+    {
+        //ComputeShader引数をセット
+        shaderGenerateSpectrum.SetBuffer(kernel_GenerateSpectrumKernel, "h0", d_h0);
+        shaderGenerateSpectrum.SetBuffer(kernel_GenerateSpectrumKernel, "ht", d_ht);
+        shaderGenerateSpectrum.SetBuffer(kernel_GenerateSpectrumKernel, "ht_dx", d_ht_dx);
+        shaderGenerateSpectrum.SetBuffer(kernel_GenerateSpectrumKernel, "ht_dz", d_ht_dz);
+        shaderGenerateSpectrum.SetBuffer(kernel_GenerateSpectrumKernel, "displaceX", d_displaceX);
+        shaderGenerateSpectrum.SetBuffer(kernel_GenerateSpectrumKernel, "displaceZ", d_displaceZ);
+        shaderGenerateSpectrum.SetInt("N", (int)Phillips.meshSize);
+        shaderGenerateSpectrum.SetInt("seasizeLx", (int)Phillips.seasizeLx);
+        shaderGenerateSpectrum.SetInt("seasizeLz", (int)Phillips.seasizeLz);
+        shaderSetNormal.SetBuffer(kernel_SetNormalBubble, "ht_dx", d_ht_dx);
+        shaderSetNormal.SetBuffer(kernel_SetNormalBubble, "ht_dz", d_ht_dz);
+        shaderSetNormal.SetBuffer(kernel_GenerateSpectrumKernel, "displaceX", d_displaceX);
+        shaderSetNormal.SetBuffer(kernel_GenerateSpectrumKernel, "displaceZ", d_displaceZ);
+        shaderSetNormal.SetTexture(kernel_SetNormalBubble, "normalTex", normal_Tex);
+        shaderSetNormal.SetTexture(kernel_SetNormalBubble, "bubbleTex", bubble_Tex);
+        shaderSetNormal.SetFloat("dx", 1.0f * Phillips.seasizeLx / Phillips.meshSize);
+        shaderSetNormal.SetFloat("dz", 1.0f * Phillips.seasizeLz / Phillips.meshSize);
+        shaderSetNormal.SetFloat("lambda", lambda);
+        shaderSetNormal.SetInt("N", (int)Phillips.meshSize);
+        // GPUバッファをマテリアルに設定
+        renderingShader_Material.SetBuffer("d_ht", d_ht);
+        renderingShader_Material.SetTexture("_MainTexN", normal_Tex);
+        renderingShader_Material.SetTexture("_MainTexB", bubble_Tex);
+        renderingShader_Material.SetBuffer("d_displaceX", d_displaceX);
+        renderingShader_Material.SetBuffer("d_displaceZ", d_displaceZ);
+        // その他Shader 定数関連
+        renderingShader_Material.SetInt("N", (int)Phillips.meshSize);
+        renderingShader_Material.SetFloat("halfN", 0.5f * Phillips.meshSize);
+        renderingShader_Material.SetFloat("dx", 1.0f * Phillips.seasizeLx / Phillips.meshSize);
+        renderingShader_Material.SetFloat("dz", 1.0f * Phillips.seasizeLz / Phillips.meshSize);
+        renderingShader_Material.SetFloat("lambda", lambda);
+    }
+
+
+    void Update()
+    {
+        Calc_Spectrum();
+        CalcFFT_ht();
+        CalcFFT_ht_dxz();
+        CalcFFT_displaceXZ();
+        SetNormal();
+        cnt++;
+        //このあとレンダリング
+    }
+
+    void Calc_Spectrum()
+    {
+        //引数tをセット
+        shaderGenerateSpectrum.SetFloat("t", 0.04f * cnt + 100.0f);
+        shaderGenerateSpectrum.Dispatch(kernel_GenerateSpectrumKernel, 1, 256, 1);//d_h0からd_htを計算、偏微分成分も
+    }
+
+    void CalcFFT_ht()
+    {
+        fFT.FFT2D_256_Dispatch(d_ht, d_ht_dmy);//d_htから高さデータを計算
+    }
+
+    void CalcFFT_ht_dxz()
+    {
+        fFT.FFT2D_256_Dispatch(d_ht_dx, d_ht_dmy);//d_ht_dxから高さデータを計算、d_ht_dxに結果が入る
+        fFT.FFT2D_256_Dispatch(d_ht_dz, d_ht_dmy);//d_ht_dzから高さデータを計算、d_ht_dzに結果が入る
+    }
+    void CalcFFT_displaceXZ()
+    {
+        fFT.FFT2D_256_Dispatch(d_displaceX, d_ht_dmy);//
+        fFT.FFT2D_256_Dispatch(d_displaceZ, d_ht_dmy);//
+    }
+
+    void SetNormal()
+    {
+        shaderSetNormal.Dispatch(kernel_SetNormalBubble, 1, 256, 1);//偏微分bufferから値を抽出し法線ベクトルを計算してtextureに保存
+    }
+
+    void OnRenderObject()
+    {
+        // レンダリングを開始
+        renderingShader_Material.SetPass(0);
+        Graphics.DrawProceduralNow(MeshTopology.Quads, (int)(Phillips.meshSize * Phillips.meshSize) * 4);
+        //Graphics.DrawProceduralNow(MeshTopology.Lines, (int)(Phillips.meshSize * Phillips.meshSize)*4);
+    }
+
+    private void OnDestroy()
+    {
+        //解放
+        d_h0.Release();
+        d_ht.Release();
+        d_ht_dx.Release();
+        d_ht_dz.Release();
+        d_ht_dmy.Release();
+        d_displaceX.Release();
+        d_displaceZ.Release();
+    }
+
+}
---
+++ OceanFFT6Bubbles/Assets/Scripts/SurfaceShader.shader
@@ -0,0 +1,94 @@
+﻿Shader "Custom/SurfaceShader" {
+	Properties{
+		_MainTexN("-" , 2D) = "black" {}
+		_MainTexB("-" , 2D) = "black" {}
+	}
+
+	SubShader{
+
+		Pass {
+			CGPROGRAM
+			// シェーダーモデルは5.0を指定
+			#pragma target 5.0
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			sampler2D _MainTexN;
+			sampler2D _MainTexB;
+	StructuredBuffer<float2> d_ht;
+	StructuredBuffer<float2> d_displaceX;
+	StructuredBuffer<float2> d_displaceZ;
+	uint N;
+	float halfN;
+	float dx;
+	float dz;
+	float lambda;
+
+	float Get_d_ht_real(uint x,uint z)
+	{
+		float2 h = d_ht[x % 256 + z % 256 * 256];
+		return h.x;
+	}
+
+	float2 Get_d_displaceXZ(uint x, uint z)
+	{
+		float2 ret;
+		ret.x = d_displaceX[x % 256 + z % 256 * 256].x;
+		ret.y = d_displaceZ[x % 256 + z % 256 * 256].x;
+		return lambda * ret;
+	}
+
+	struct VSOut {
+		float4 pos : SV_POSITION;
+		float3 pos2 : TEXCOORD2;
+		float2 uv : TEXCOORD0;
+	};
+
+	// 頂点シェーダ、1つの4角形ポリゴンに頂点4つ
+	VSOut vert(uint id : SV_VertexID,float3 normal : NORMAL)
+	{
+		VSOut output;
+		uint sqx = ((id + 1) % 4 / 2);//連続するid4つで四角形を作る
+		uint sqz = (1 - id % 4 / 2);
+		sqx += (id / 4) % 256;
+		sqz += (id / 4) / 256;
+		output.pos.y = Get_d_ht_real(sqx, sqz);
+		output.pos.xz = float2((sqx - halfN) * dx, (sqz - halfN) * dz);//4角形
+		output.pos.xz += Get_d_displaceXZ(sqx, sqz);
+		output.pos.w = 1;
+		output.pos2 = output.pos.xyz;
+		output.pos = mul(UNITY_MATRIX_VP, output.pos);
+		float rN1 = 1.0 / N;
+		output.uv = float2(sqx, sqz) * rN1 + float2(0.5, 0.5) * rN1;
+		return output;
+	}
+	
+	// ピクセルシェーダー
+	// ワールド座標系がよくわからないのでレイトレーシング的に色を決定している(要修正)
+	float4 frag(VSOut i) : COLOR
+	{
+		float4 col; col.w = 1;
+		float3 normal = normalize(tex2D(_MainTexN, i.uv).xyz);//線形補完されて単位ベクトルじゃなくなっているので
+		float3 viewDir = normalize(i.pos2 - float3(0, 41, -330));//ここはMain Cameraの座標を埋め込み
+		float3 lightDir = normalize(float3(0.15, 0.45, 0.6));//ライトベクトルも埋め込み
+		float3 reflectDir = -2.0 * dot(normal, viewDir) * normal + viewDir;
+		float v = dot(reflectDir, lightDir);
+		float3 sky = (v + 1.0) * float3(105.0 / 256, 133.0 / 256, 184.0 / 256);//空の色も埋め込み、しかも単色
+		float fresnel = (0.05 + (1 - 0.05) * pow(1 - max(dot(normal, -viewDir),0), 5));
+		col.xyz = sky * fresnel + (1.0 - fresnel) * float3(0.01, 0.13, 0.15);//海の色も埋め込み
+		col.xyz += pow(max(v, 0), 649) * float3(1.2, 1.0, 0.86);//太陽の色も埋め込み
+		
+		//泡
+		float bubble = tex2D(_MainTexB, i.uv);
+		if (bubble < -0.3) {
+			bubble = min(-bubble * 0.4, 1);//裏裏返り度合いに応じて徐々に白くなる
+			col.xyz = bubble * float3(1.0, 1.0, 1.0) + (1.0 - bubble) * col.xyz;
+		}
+		return col;
+	}
+
+	ENDCG
+ }
+	}
+}
